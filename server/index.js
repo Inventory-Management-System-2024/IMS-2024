8dfe8a9ead2cb2384bfffc5cc88c39d3e3d8a273
MansiBavishi <mansibavishiinventyv@gmail.com>
2024-01-17 16:05:44 +0530

    add readme.md in pipes - MB

--- server/index.js
+++ server/index.js
@@ -1,13 +1,12 @@
-import express from "express"
-import env from 'dotenv';
+import express from "express";
+import env from "dotenv";
 import dbConnect from "./db/index.js";
 
-env.config()
-const app = express()
+env.config();
+const app = express();
 
 dbConnect();
 
-
-app.listen(3000,()=>{
-    console.log("Listening on 3000")
-})
+app.listen(3000, () => {
+  console.log("Listening on 3000");
+});
