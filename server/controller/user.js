76eddbaff3640d104e569f8596a8678927a01ba6
Jash Jasani <69423090+jashjasani@users.noreply.github.com>
2024-01-17 16:16:03 +0530

    Fixed imports (#37) - jcube
    Co-authored-by: est091 <ellis0078@gmail.com>
    Co-authored-by: Jay Shah <86820052+jayshah2002@users.noreply.github.com>
    Co-authored-by: jash-inventyv <jash.inventyv@gmail.com>
    Co-authored-by: mv-here <munnavermainventyv@gmail.com>
    Co-authored-by: Munna Verma <153286992+mv-here@users.noreply.github.com>

---
+++ server/controller/user.js
@@ -0,0 +1,25 @@
+import { User } from "../models";
+function createUser(){ 
+    
+}
+
+function getUser(){
+    
+}
+
+function updateUser(){
+
+}
+
+function deleteUser(){
+
+}
+
+function getAllUser(){
+
+}
+
+
+function register(){
+
+}
