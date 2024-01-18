7d2c4aa487e5f9a90b40691d5589b09413acbc40
khushijaiswal23 <khushijaiswalinventyv@gmail.com>
2024-01-18 13:54:02 +0530

    Created a Dashboard Page - KJ

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { ProductsComponent } from './pages/products/products.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet,RouterLink, DashboardComponent,ProductsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
