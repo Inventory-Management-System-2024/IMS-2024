21c10f9651e9f19b538655680799c2ceb88548e7
khushijaiswal23 <khushijaiswalinventyv@gmail.com>
2024-01-19 19:00:51 +0530

    Created add-Product page -KJ

--- client/src/app/pages/add-product/add-product.component.ts
+++ client/src/app/pages/add-product/add-product.component.ts
@@ -1,11 +1,19 @@
 import { Component, Output, EventEmitter } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+// import { MatTextareaAutosize } from '@angular/material/textarea';
+
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInput } from '@angular/material/input';
+// import { CdkTextareaAutosize } from '@angular/cdk/text-field';
+
 
 @Component({
   selector: 'app-add-product',
   standalone: true,
-  imports: [RouterLink,RouterLinkActive, FormsModule],
+  imports: [RouterLink,RouterLinkActive, FormsModule, MatButtonModule, MatInputModule, MatFormFieldModule, MatInput,],
   templateUrl: './add-product.component.html',
   styleUrl: './add-product.component.css'
 })
