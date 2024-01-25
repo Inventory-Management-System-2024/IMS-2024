e4b4e3d70d40ed6e3657ef6a80f878cb91ab4017
SushilInventyv <153286178+SushilInventyv@users.noreply.github.com>
2024-01-25 12:41:01 +0530

    Merged and Approved by - ET
    Co-authored-by: est091 <ellis0078@gmail.com>
    Co-authored-by: Jay Shah <86820052+jayshah2002@users.noreply.github.com>
    Co-authored-by: jash-inventyv <jash.inventyv@gmail.com>
    Co-authored-by: Jash Jasani <69423090+jashjasani@users.noreply.github.com>
    Co-authored-by: mv-here <munnavermainventyv@gmail.com>
    Co-authored-by: Munna Verma <153286992+mv-here@users.noreply.github.com>
    Co-authored-by: jashjasani <jashjjj3@gmail.com>
    Co-authored-by: HirparaSEdit <anadihirpara0036@gmail.com>
    Co-authored-by: MansiBavishi <mansibavishiinventyv@gmail.com>
    Co-authored-by: Namra-23 <namrajoshi2303@gmail.com>
    Co-authored-by: mbavishi <mansibavishi640@gmail.com>
    Co-authored-by: jayshahinventyv <jayshahinventyv@gmail.com>
    Co-authored-by: aditya-inventyv <adityapatelinventyv@gmail.com>
    Co-authored-by: virenribadiya <108461765+virenribadiya@users.noreply.github.com>
    Co-authored-by: khushijaiswal23 <khushijaiswalinventyv@gmail.com>
    Co-authored-by: aditya-inventyv <154305722+aditya-inventyv@users.noreply.github.com>
    Co-authored-by: khushijaiswal23 <153286996+khushijaiswal23@users.noreply.github.com>
    Co-authored-by: Mansi Bavishi <98725200+mbavishi@users.noreply.github.com>
    Co-authored-by: Namra Joshi <85553200+Namra-23@users.noreply.github.com>

--- client/src/app/pages/login/login.component.ts
+++ client/src/app/pages/login/login.component.ts
@@ -1,41 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { Router } from '@angular/router';
-import { ReactiveFormsModule, FormControl, FormGroup, Validators, FormsModule } from '@angular/forms';
+import {
+  ReactiveFormsModule,
+  FormControl,
+  FormGroup,
+  Validators,
+  FormsModule,
+} from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
 export class LoginComponent {
-
   public showPassword: boolean = false;
+  toast = inject(ToastrService);
   constructor(private route: Router) { }
   getUser(_email: string, _password: string) {
-    console.log("Logged in Successfully");
-    if (_email != "" && _password != "") {
-      alert("Login SuccessFull");
+    if (_email != '' && _password != '') {
+      this.toast.success("Login SuccessFul!", "Success");
       this.route.navigate(['dashboard']);
-    } else {
-      alert("Invalid Username or Password!")
-    }
-
+    } 
+    
   }
   signUp() {
     this.route.navigate(['register']);
   }
   loginForm = new FormGroup({
     email: new FormControl('', [Validators.required, Validators.email]),
-    password: new FormControl('', [Validators.required, Validators.pattern("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).{8,10}$")])
+    password: new FormControl('', [
+      Validators.required,
+      Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).{8,15}$'),
+    ]),
   });
 
   togglePassword(): void {
     this.showPassword = !this.showPassword;
   }
-
 
   get pass() {
     return this.loginForm.get('password');
