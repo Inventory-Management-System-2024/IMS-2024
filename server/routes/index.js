b72fdeabe22b102a20b624f471de331106ea71f4
Ellis Sanjay Tarmaster <ellis0078@gmail.com>
2024-01-29 14:41:03 +0530

    Add Directives & Pipes - MB (#133) (#135)
    Co-authored-by: Jay Shah <86820052+jayshah2002@users.noreply.github.com>
    Co-authored-by: jash-inventyv <jash.inventyv@gmail.com>
    Co-authored-by: Jash Jasani <69423090+jashjasani@users.noreply.github.com>
    Co-authored-by: mv-here <munnavermainventyv@gmail.com>
    Co-authored-by: Munna Verma <153286992+mv-here@users.noreply.github.com>
    Co-authored-by: jashjasani <jashjjj3@gmail.com>
    Co-authored-by: HirparaSEdit <anadihirpara0036@gmail.com>
    Co-authored-by: MansiBavishi <mansibavishiinventyv@gmail.com>
    Co-authored-by: Namra-23 <namrajoshi2303@gmail.com>
    Co-authored-by: mbavishi <mansibavishi640@gmail.com>
    Co-authored-by: jayshahinventyv <jayshahinventyv@gmail.com>
    Co-authored-by: aditya-inventyv <adityapatelinventyv@gmail.com>
    Co-authored-by: Sushil <sushilgangavatiinventyv@gmail.com>
    Co-authored-by: SushilInventyv <153286178+SushilInventyv@users.noreply.github.com>
    Co-authored-by: virenribadiya <108461765+virenribadiya@users.noreply.github.com>
    Co-authored-by: khushijaiswal23 <khushijaiswalinventyv@gmail.com>
    Co-authored-by: aditya-inventyv <154305722+aditya-inventyv@users.noreply.github.com>
    Co-authored-by: khushijaiswal23 <153286996+khushijaiswal23@users.noreply.github.com>
    Co-authored-by: Mansi Bavishi <98725200+mbavishi@users.noreply.github.com>
    Co-authored-by: Namra Joshi <85553200+Namra-23@users.noreply.github.com>

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,14 +1,6 @@
-import express from "express"
-
-
-const router = express.Router()
-
 import { user_router } from "./user.js";
 import { login_router } from "./login.js";
 import { order_router } from "./order.js";
 import { product_router } from "./product.js";
 
 export { user_router, login_router, order_router, product_router };
-
-
-export {user_router}
