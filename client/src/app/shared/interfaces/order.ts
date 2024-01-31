def9bbd18e17c78dfcf891f7d3f2f6e2ce236540
HirparaSEdit <anadihirpara0036@gmail.com>
2024-01-31 13:42:05 +0530

    added Product,order,user interface

---
+++ client/src/app/shared/interfaces/order.ts
@@ -0,0 +1,16 @@
+  export default interface OrderElement {
+    user: {
+      name: String,
+      email: String
+    };
+    orderItems: [{
+      quantity: number,
+      product: {
+        productName: String,
+      }
+      _id: string
+    }],
+    orderStatus: string;
+    totalPrice: number;
+    paidAt: String;
+  }
