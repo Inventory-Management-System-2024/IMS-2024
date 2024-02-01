b2e38366a072d732ea2387d6fc8c167578fee87a
aditya-inventyv <adityapatelinventyv@gmail.com>
2024-02-01 16:38:01 +0530

    intregated product search api into frontend

--- client/src/app/shared/services/product.service.ts
+++ client/src/app/shared/services/product.service.ts
@@ -21,16 +21,15 @@ export class ProductService {
       catchError(this.err.handleError)
     );
   }
-  getProduct(id: number): Observable<Product>{
-    return this.http.get<Product>(`${this._url}/${id}`,{ headers: this.headers }).pipe(
-
-      catchError(this.err.handleError)
-    );
-  }  
-  addProduct(product : Product): Observable<Product>{
-    return this.http.post<Product>(`${this._url}`, product, { headers: this.headers }).pipe(
-      catchError(this.err.handleError)
-    );
+  getProduct(name: string): Observable<Product[]> {
+    return this.http
+      .get<Product[]>(`${this._url}/${name}`, { headers: this.headers })
+      .pipe(catchError(this.err.handleError));
+  }
+  addProduct(product: Product): Observable<Product> {
+    return this.http
+      .post<Product>(`${this._url}`, product, { headers: this.headers })
+      .pipe(catchError(this.err.handleError));
   }
   updateProduct(id : number, product : Product): Observable<Product>{
     return this.http.put<Product>(`${this._url}/${id}`,product,{ headers: this.headers }).pipe(
