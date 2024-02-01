d242d92564868d68176a05a3550a99ecf2c21035
aditya-inventyv <154305722+aditya-inventyv@users.noreply.github.com>
2024-02-01 18:45:06 +0530

    intregated product search api into frontend -adi (#173)
    updated files
    
    product-list.component.html
    product-list.component.ts
    services\product.service.ts
    
    ---------
    
    Co-authored-by: jashjasani <jashjjj3@gmail.com>
    Co-authored-by: mbavishi <mansibavishi640@gmail.com>
    Co-authored-by: khushijaiswal23 <khushijaiswalinventyv@gmail.com>
    Co-authored-by: Mansi Bavishi <98725200+mbavishi@users.noreply.github.com>
    Co-authored-by: khushijaiswal23 <153286996+khushijaiswal23@users.noreply.github.com>

---
+++ client/src/app/pages/order/update-dialog/update-dialog.component.ts
@@ -0,0 +1,31 @@
+import { Component, Inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatDialogRef, MatDialogModule, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MatFormField, MatFormFieldModule, MatLabel } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+
+@Component({
+  selector: 'app-update-dialog',
+  standalone: true,
+  imports: [MatDialogModule, MatFormFieldModule, MatInputModule, MatSelectModule, MatLabel, MatFormField, FormsModule],
+  templateUrl: './update-dialog.component.html',
+  styleUrl: './update-dialog.component.css'
+})
+
+export class UpdateDialogComponent {
+  selectedOption!: string;
+
+  constructor(
+    public dialogRef: MatDialogRef<UpdateDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any
+  ) { }
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+
+  onYesClick(): void {
+    this.dialogRef.close(this.selectedOption);
+  }
+}
